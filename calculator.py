46632bc0d11a0c63195d6ae22c499631e12a0819
Sergio Puente Castro <sergiopuentecastro@MacBook-Air-de-Sergio.local>
2020-11-25 22:23:29 +0100

    Distintas formas de crear la calculadora

--- calculator.py
+++ calculator.py
@@ -4,6 +4,103 @@ from tkinter import ttk
 WIDTH = 68
 HEIGHT = 50
 
+botones = (("C", 1), ("+/-", 1), ("%", 1), ("÷", 1), ("7", 1), ("8", 1), ("9", 1), ("x", 1), ("4", 1), ("5", 1), ("6", 1), ("-", 1), ("1", 1), ("2", 1), ("3", 1), ("+", 1), ("0", 2), (",", 1), ("=", 1))
+coordenadas =[]
+
+dbotones = [
+    {
+        'text': 'C',
+        'r': 0,
+        'c': 1,
+    },
+    {
+        'text': '+/-',
+        'r': 0,
+        'c': 2,
+    },
+    {
+        'text': '÷',
+        'r': 0,
+        'c': 3,
+    },
+    {
+        'text': '7',
+        'r': 1,
+        'c': 0,
+    },
+    {
+        'text': '8',
+        'r': 1,
+        'c': 1,
+    },
+    {
+        'text': '9',
+        'r': 1,
+        'c': 2,
+    },
+    {
+        'text': 'x',
+        'r': 1,
+        'c': 3,
+    },
+    {
+        'text': '4',
+        'r': 2,
+        'c': 0,
+    },
+    {
+        'text': '5',
+        'r': 2,
+        'c': 1,
+    },
+    {
+        'text': '6',
+        'r': 2,
+        'c': 2,
+    },
+    {
+        'text': '-',
+        'r': 2,
+        'c': 3,
+    },
+    {
+        'text': '1',
+        'r': 3,
+        'c': 0,
+    },
+    {
+        'text': '2',
+        'r': 3,
+        'c': 1,
+    },
+    {
+        'text': '3',
+        'r': 3,
+        'c': 2,
+    },
+    {
+        'text': '+',
+        'r': 3,
+        'c': 3,
+    },
+    {
+        'text':'0',
+        'r':4,
+        'c':0,
+        'w': 2
+    },
+    {
+        'text': ',',
+        'r': 4,
+        'c': 2,
+    },
+    {
+        'text': '=',
+        'r': 4,
+        'c': 3,
+    },
+]
+
 class Display(ttk.Frame):
 
     def __init__(self, parent):
@@ -24,3 +121,36 @@ class CalcButton(ttk.Frame):
 
         ttk.Button(self, text=text, command=command).pack(side=TOP, fill=BOTH, expand=True) # anteriormente creo un boton, y con esto lo que hacemos es que esto sea el boton
         #btn.pack(side=TOP, fill=BOTH, expand=True)
+
+class Keyboard(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, widt=WIDTH*4, height=HEIGHT )
+        self.pack_propagate(0)
+        s = ttk.Style()
+        s.theme_use("alt")
+
+        coordenadas = []
+        for fila in range(5):
+            for columna in range(4):
+                coordenadas.append((fila, columna))
+        print(coordenadas)
+
+        k = 0
+        for tecla in botones:
+            boton = CalcButton(self, tecla)
+            boton.grid(row=coordenadas[k][0], column=coordenadas[k][1], columnspan=ancho)
+            k += ancho
+
+class Keyboard_con_diccionario(ttk.Frame):
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, widt=WIDTH*4, height=HEIGHT )
+        self.pack_propagate(0)
+        s = ttk.Style()
+        s.theme_use("alt")
+
+        for boton in dbotones:
+            w = boton.get("w", 1)
+            h = boton.get("h", 1)
+
+            btn = CalcButton(self, boton["text"],width=w, height=h)
+            btn.grid(row=boton["r"], column=boton["c"], columnspan=w, rowspan=h)
--- main.py
+++ main.py
@@ -14,43 +14,16 @@ class MainApp(Tk):
         self.title("Calculadora")
 
         self.display = calculator.Display(self)
-        self.display.pack(side=TOP)
+        self.display.pack(side=TOP, fill=BOTH, expand=True)
 
-        self.teclado = ttk.Frame(self, width=calculator.WIDTH*4, height=calculator.HEIGHT * 5)
-        self.teclado.grid_propagate(0)#con esto lo que conseguimos es que salga toda la pantalla, y no solo los botones que hemos creado
-        self.teclado.pack(side=TOP, fill=BOTH, expand=True)
+        self.teclado = calculator.Keyboard_con_diccionario(self)
+        self.teclado.pack(side=TOP)
 
-        botonC = calculator.CalcButton(self.teclado, "C")
-        botonC.grid(row=0, column=0)
+        #self.teclado = ttk.Frame(self, width=calculator.WIDTH*4, height=calculator.HEIGHT * 5)
+        #self.teclado.grid_propagate(0)#con esto lo que conseguimos es que salga toda la pantalla, y no solo los botones que hemos creado
+        #self.teclado.pack(side=TOP, fill=BOTH, expand=True)
+
         
-        botonC = calculator.CalcButton(self.teclado, "+/-")
-        botonC.grid(row=0, column=1)
-
-        botonC = calculator.CalcButton(self.teclado, "÷")
-        botonC.grid(row=0, column=2)
-
-        botonC = calculator.CalcButton(self.teclado, "+")
-        botonC.grid(row=0, column=3)
-
-        """
-        self.calcButtonC = ttk.Frame(self, width=136, height=50)
-        btn = ttk.Button(self.calcButtonC, text="C")
-        self.calcButtonC.pack_propagate(False)
-        btn.pack(side=TOP, fill=BOTH, expand=True)
-        self.calcButtonC.grid(column=0, row=1, columnspan=2)
-
-        self.calcButtonCs = ttk.Frame(self, width=68, height=50)
-        btn = ttk.Button(self.calcButtonCs, text="+/-")
-        self.calcButtonCs.pack_propagate(False)
-        btn.pack(side=TOP, fill=BOTH, expand=True)
-        self.calcButtonCs.grid(column=2, row=1)
-        
-        self.calcButtondiv = ttk.Frame(self, width=68, height=50)
-        btn = ttk.Button(self.calcButtondiv, text="÷")
-        self.calcButtondiv.pack_propagate(False)
-        btn.pack(side=TOP, fill=BOTH, expand=True)
-        self.calcButtondiv.grid(column=3, row=1)
-        """
 
 if __name__ == "__main__":
     app = MainApp()
